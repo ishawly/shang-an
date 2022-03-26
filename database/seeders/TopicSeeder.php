bb7dff42eb2e6ffcb443832a904fc241bccd222a
liber <liber@shawly.cn>
2022-03-26 18:00:19 +0800

    add topic and user db seeder

---
+++ database/seeders/TopicSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TopicSeeder extends Seeder
+{
+    private $tableName = 'topics';
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $topic = DB::table($this->tableName)->find(1);
+        if (! $topic) {
+            DB::table('topics')->insert([
+                'id' => 1,
+                'topic_name' => '结伴学习',
+                'remarks' => '结伴学习',
+                'created_by' => 1
+            ]);
+        }
+    }
+}
