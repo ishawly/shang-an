8515a8a1fe08bbdce8b4fa798d5cca075e01bc0e
liber <liber@shawly.cn>
2022-04-26 22:54:50 +0800

    customize php cs fixer rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -20,4 +20,15 @@ return $config->setRules([
     'binary_operator_spaces' => [
         'default' => 'align_single_space_minimal',
     ],
+    // imports
+    'no_unused_imports' => false,
+    // trait
+    'single_trait_insert_per_statement' => false,
+    // phpdoc
+    'no_superfluous_phpdoc_tags' => false,
+    'phpdoc_separation'          => false,
+    // string
+    'concat_space' => ['spacing' => 'one'],    // none, one
+    // php unit
+    'php_unit_method_casing' => ['case' => 'snake_case'], // default: camel_case
 ])->setFinder($finder);
