4058eae7663a3f2d52e79acd41c391808030c7e8
liber <liber@shawly.cn>
2022-04-28 15:35:37 +0800

    WeChat mini app login

--- app/Http/Controllers/Wechat/MiniAppController.php
+++ app/Http/Controllers/Wechat/MiniAppController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Wechat;
 
+use App\Models\ThirdApp;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Overtrue\LaravelWeChat\EasyWeChat;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,10 +32,35 @@ class MiniAppController extends BaseController
             ]);
             $data = $response->toArray();
             if (empty($data['openid'])) {
-                return $this->error($data['errmsg'], Response::HTTP_SERVICE_UNAVAILABLE);
+                throw new \Exception($data['errmsg']);
             }
 
-            return $this->success($response->toArray());
+            $thirdApp = ThirdApp::query()->firstOrCreate([
+                'openid'   => $data['openid'],
+                'app_type' => ThirdApp::APP_TYPE_WECHAT_MINI_APP,
+            ], [
+                'unionid'     => empty($data['unionid']) ? '' : $data['unionid'],
+                'session_key' => $data['session_key'],
+            ]);
+            if (empty($thirdApp->user_id)) {
+                $user = User::create([
+                    'name'     => '',
+                    'email'    => md5($data['openid']) . '@shang-an.shawly.cn',
+                    'password' => '',
+                ]);
+                $thirdApp->user_id = $user->id;
+                $thirdApp->update();
+            }
+
+            auth()->login($thirdApp->user);
+            $user  = auth()->user();
+            $token = $user->createToken('api');
+
+            return $this->success([
+                'access_token' => $token->plainTextToken,
+                'token_type'   => 'bearer',
+                // 'expires_in' => 60, // TODO:: fill right expire time
+            ]);
         } catch (\Throwable $throwable) {
             return $this->error($throwable->getMessage(), Response::HTTP_SERVICE_UNAVAILABLE);
         }
